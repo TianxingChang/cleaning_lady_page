df3ba7110316ca4effc19dfdcb01c2c1d89547f5
Matroid_Walker <adimensioner@gmail.com>
2023-05-28 23:41:27 +0800

    Janitor Page expect api

---
+++ src/pages/Home.js
@@ -0,0 +1,65 @@
+import { cloneElement, useEffect, useState } from "react";
+import "../styles/App.css";
+import Card from "./Card";
+import { useLocation } from "react-router-dom";
+
+export default function Home() {
+  const toilet_list = [
+    ["lg1-a", "Deep Cleaning"],
+    ["lg3-c", "floor cleaning"],
+    ["lg4-b", "Cubicle 2"],
+    ["g-a", "Cubicle 3"],
+  ];
+  const [style, setStyle] = useState("cleaning_card");
+  const [id, setId] = useState(null);
+  const location = useLocation();
+
+  // useEffect(() => {
+  //   const isOn = location.state ? location.state.isOn : null;
+
+  //   const this_id = location.state ? location.state.id : null;
+
+  //   if (isOn) {
+  //     setStyle("cleaning_card2");
+  //     if (this_id) {
+  //       setId(this_id);
+  //     }
+  //   } else {
+  //     setStyle("cleaning_card");
+  //   }
+  // }, [location.state]);
+
+  const clean_card = toilet_list.map((toilet) => (
+    <Card
+      info={toilet}
+      this_style={style}
+      id={
+        localStorage.getItem(toilet[0] + "_card")
+          ? localStorage.getItem(toilet[0] + "_card")
+          : "cleaning_card"
+      }
+    />
+  ));
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Cleaning List</h1>
+      </header>
+      <div className="info">
+        <h3> </h3>
+      </div>
+      <div className="main">
+        <div className="container">{clean_card}</div>
+      </div>
+    </div>
+  );
+}
+
+// function CardWrapper({ card }) {
+//   const location = useLocation();
+//   const isOn = location.state ? location.state.isOn : null;
+//   const id = isOn ? isOn[1] : null;
+
+//   return cloneElement(card, { id: id });
+// }
